f09c3eebbb07592c498931b8505b073ae5073e60
rjfeeney <rileyfeeney7@gmail.com>
2025-04-01 16:59:42 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+}
+
+type parameters struct {
+	Body string `json:"body"`
+}
+
+type validResp struct {
+	Cleaned_body string `json:"cleaned_body"`
 }
 
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
@@ -41,6 +51,51 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Hits have been reset")
 }
 
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	response := map[string]string{"error": msg}
+	json.NewEncoder(w).Encode(response)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(payload)
+}
+
+func profanityCheck(msg string) string {
+	wordsList := strings.Fields(msg)
+	var finishedlist []string
+	for _, word := range wordsList {
+		if strings.ToLower(word) == "kerfuffle" || strings.ToLower(word) == "sharbert" || strings.ToLower(word) == "fornax" {
+			word = "****"
+		}
+		finishedlist = append(finishedlist, word)
+	}
+	combined := strings.Join(finishedlist, " ")
+	return combined
+}
+
+func (cfg *apiConfig) validateHandler(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	unmarshErr := decoder.Decode(&params)
+	if unmarshErr != nil {
+		respondWithError(w, 400, "Failed to decode request body")
+		return
+	}
+	if len(params.Body) > 140 {
+		respondWithError(w, 400, "Chirp exceeds 140 characters")
+		return
+	}
+	cleaned := profanityCheck(params.Body)
+	valid := validResp{
+		Cleaned_body: cleaned,
+	}
+	respondWithJSON(w, 200, valid)
+}
+
 func main() {
 	cfg := &apiConfig{}
 	mux := http.NewServeMux()
@@ -55,5 +110,6 @@ func main() {
 	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
 	mux.HandleFunc("GET /admin/metrics", cfg.metricsHandler)
 	mux.HandleFunc("POST /admin/reset", cfg.resetHandler)
+	mux.HandleFunc("POST /api/validate_chirp", cfg.validateHandler)
 	log.Fatal(newServer.ListenAndServe())
 }
